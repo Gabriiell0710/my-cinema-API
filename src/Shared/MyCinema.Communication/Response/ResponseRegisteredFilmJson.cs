eda988421b64935e07d6c08f3e5b767663eae668
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 13:34:01 -0300

    feat: created response register film json (DTO)

---
+++ src/Shared/MyCinema.Communication/Response/ResponseRegisteredFilmJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Communication.Response
+{
+    public class ResponseRegisteredFilmJson
+    {
+        public string Title { get; set; } = String.Empty;
+    }
+}
