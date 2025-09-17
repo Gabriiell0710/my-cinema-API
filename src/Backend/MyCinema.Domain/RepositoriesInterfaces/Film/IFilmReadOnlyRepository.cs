81ab39ce50b9ea095ff7a576d77146894f859470
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 13:54:46 -0300

    feat: created film interfaces repositories for write and read operations

---
+++ src/Backend/MyCinema.Domain/RepositoriesInterfaces/Film/IFilmReadOnlyRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyCinema.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Domain.RepositoriesInterfaces.Film
+{
+    public interface IFilmReadOnlyRepository
+    {
+        Task<List<FilmModel>> FindAllFilms();
+        Task<FilmModel> FindFilmById(int id);
+
+    }
+}
---
+++ src/Backend/MyCinema.Domain/RepositoriesInterfaces/Film/IFilmWriteOnlyRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyCinema.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Domain.RepositoriesInterfaces.Film
+{
+    public interface IFilmWriteOnlyRepository
+    {
+        Task<FilmModel> AddFilm(FilmModel film);
+        Task<FilmModel> UpdateFilm(FilmModel film, int id);
+        Task<bool> DeleteFilm(int id);
+    }
+}
