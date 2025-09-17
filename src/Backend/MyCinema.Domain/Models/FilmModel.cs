5b1f9731f41584b4f84f9a9a71a814437d6cec2f
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 13:22:17 -0300

    feat: removed cached files(.vs) and created film model

---
+++ src/Backend/MyCinema.Domain/Models/FilmModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCinema.Domain.Models
+{
+    public class FilmModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Gender { get; set; }
+        public int Duration { get; set; }
+        public string? Classification { get; set; }
+
+    }
+}
