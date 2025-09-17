5a2b52f142ff2bc7c356c5ce91324c8ebcaf058a
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 13:30:35 -0300

    feat: created request register film json (DTO)

---
+++ src/Shared/MyCinema.Communication/Requests/RequestRegisterFilmJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Communication.Requests
+{
+    public class RequestRegisterFilmJson
+    {
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public int Duration { get; set; }
+        public string Classification {  get; set; } 
+    }
+}
