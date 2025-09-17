d94638f8baa167ff70c974effdb88c590a214e29
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 16:17:11 -0300

    feat: created migration

--- src/Backend/MyCinema.API/Program.cs
+++ src/Backend/MyCinema.API/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using MyCinema.Domain.RepositoriesInterfaces.Film;
+using MyCinema.Infrastructure.DataAcess;
+using MyCinema.Infrastructure.Repositories.Film;
+
 namespace MyCinema.API
 {
     public class Program
@@ -13,6 +18,14 @@ namespace MyCinema.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddEntityFrameworkSqlServer()
+                .AddDbContext<MyCinemaDbContex>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database"))
+                );
+
+            builder.Services.AddScoped<IFilmReadOnlyRepository, FilmRepository>();
+            builder.Services.AddScoped<IFilmWriteOnlyRepository, FilmRepository>();
 
             var app = builder.Build();
 
---
+++ src/Backend/MyCinema.Infrastructure/Migrations/20250917190646_InitialDb.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCinema.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Classification = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
