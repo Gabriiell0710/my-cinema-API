11de4cfa5084c07e749185d158bb95842148ad07
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-17 14:08:44 -0300

    feat: crated DbContext

---
+++ src/Backend/MyCinema.Infrastructure/DataAcess/MyCinemaDbContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCinema.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Infrastructure.DataAcess
+{
+    public class MyCinemaDbContex : DbContext 
+    {
+        public MyCinemaDbContex(DbContextOptions<MyCinemaDbContex> options) : base(options) { }
+
+        public DbSet<FilmModel> Film { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Backend/MyCinema.Infrastructure/Repositories/Film/FilmRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyCinema.Domain.RepositoriesInterfaces.Film;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Infrastructure.Repositories.Film
+{
+    public class FilmRepository : IFilmReadOnlyRepository, IFilmWriteOnlyRepository
+    {
+    }
+}
