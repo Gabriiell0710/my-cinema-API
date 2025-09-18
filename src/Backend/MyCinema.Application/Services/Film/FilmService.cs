99a5769d4f351d724faac66a7c8d548585585cc0
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-18 19:16:12 -0300

    feat: created film service

---
+++ src/Backend/MyCinema.Application/Services/Film/FilmService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MyCinema.Communication.Requests;
+using MyCinema.Communication.Response;
+using MyCinema.Domain.Models;
+using MyCinema.Domain.RepositoriesInterfaces.Film;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Application.Services.Film
+{
+    public class FilmService : IFIlmService
+    {
+        private readonly IFilmReadOnlyRepository _filmReadOnlyRepository;
+        private readonly IFilmWriteOnlyRepository _filmWriteOnlyRepoditory;
+        private readonly IMapper _mapper;
+
+        public FilmService (IFilmReadOnlyRepository filmReadOnlyRepository, IFilmWriteOnlyRepository filmWriteOnlyRepoditory, IMapper mapper)
+        {
+            _filmReadOnlyRepository = filmReadOnlyRepository;
+            _filmWriteOnlyRepoditory = filmWriteOnlyRepoditory;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseRegisteredFilmJson> AddFilm(RequestRegisterFilmJson request)
+        {
+            var film = _mapper.Map<FilmModel>(request);
+
+            await _filmWriteOnlyRepoditory.AddFilm(film);
+
+            return new ResponseRegisteredFilmJson
+            {
+                Title = request.Title,
+            };
+        }
+    }
+}
---
+++ src/Backend/MyCinema.Application/Services/Film/IFIlmService.cs
@@ -0,0 +1,15 @@
+﻿using MyCinema.Communication.Requests;
+using MyCinema.Communication.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCinema.Application.Services.Film
+{
+    public interface IFIlmService
+    {
+        public Task<ResponseRegisteredFilmJson> AddFilm(RequestRegisterFilmJson request);
+    }
+}
