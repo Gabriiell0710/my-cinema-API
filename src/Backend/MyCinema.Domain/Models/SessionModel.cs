e70cd4444f04676c8a5ffe76af893d37147c0610
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-25 10:33:14 -0300

    feat: created session model

---
+++ src/Backend/MyCinema.Domain/Models/SessionModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MyCinema.Domain.Models
+{
+    public class SessionModel
+    {
+        public int Id { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int FilmId { get; set; }
+        public int RoomId { get; set; }
+
+
+        public virtual FilmModel Film { get; set; }
+        public virtual RoomModel Room { get; set; }
+    }
+}
