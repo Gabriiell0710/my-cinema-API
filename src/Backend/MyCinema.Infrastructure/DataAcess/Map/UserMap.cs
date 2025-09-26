7caeee6aa51b1dcd646776f0300d86fe3038c470
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-26 13:09:57 -0300

    feat: created user map

---
+++ src/Backend/MyCinema.Infrastructure/DataAcess/Map/UserMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyCinema.Domain.Models;
+
+namespace MyCinema.Infrastructure.DataAcess.Map
+{
+    public class UserMap : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Password).IsRequired().HasMaxLength(2000);
+            builder.Property(x => x.Profile).IsRequired();
+            builder.Property(x => x.SessionID);
+
+            builder.HasOne(x => x.Session);
+           
+        }
+    }
+}
