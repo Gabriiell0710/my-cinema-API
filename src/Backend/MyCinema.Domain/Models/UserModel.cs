3144e882c69a5c858ed3e3363b4662662a4a8fa7
Gabriel Carvalho <gabriiell0710@gmail.com>
2025-09-26 13:01:44 -0300

    feat: created user model

---
+++ src/Backend/MyCinema.Domain/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using MyCinema.Domain.Enums;
+
+namespace MyCinema.Domain.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ProfileEnum Profile { get; set; }
+        public int? SessionID { get; set; } 
+
+
+        public virtual SessionModel Session { get; set; }
+
+    }
+}
